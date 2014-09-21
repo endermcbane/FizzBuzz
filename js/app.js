c1b36a15a1e0aab6264e5ef93d4c8c1914fbcc44
endermcbane <kccrookston@gmail.com>
2014-09-21 17:22:46 -0600

    refactored changes

--- js/app.js
+++ js/app.js
@@ -1,57 +1,46 @@
 $(document).ready(function(){
-	$("#start").click (function(){
+	
+	
+	var fizzy = function () {
+		var start = 1
+		var end = $('#entry').val();
 		$('#build p').remove();
+		if (($('#entry').val()) > 100 || ($('#entry').val()) < 0 || ($('#entry').val()) % 1 !=0 ){
+			alert('Make sure you enter a whole number between 1 and 100');
+		}
 
-			var start = $('#entry').val();
-			var end = 100;
-			
-			
-
-			for(i=start; i < end; i++){
-				if (i%3 == 0 && i%5 == 0){
-					$('#build').append('<p>FizzBuzz<p>');
-				}
-				else if (i%3==0){
-					$('#build').append('<p>Fizz<p>');
-				}
-				else if (i%5 == 0){
-					$('#build').append('<p>Buzz<p>');
-				}
-				else{
-					$('#build').append('<p>'+i+'<p>');
-				}
-			};
-			$('#entry').val("");
-		});
-	$('#inner').keypress(function(e){
-    if(e.which == 13){
-    	$('#build p').remove();
-    	var start = $('#entry').val();
-			var end = 101;
-			
-			
-
-			for(i=start; i < end; i++){
-				if (i%3 == 0 && i%5 == 0){
-					$('#build').append('<p>FizzBuzz<p>');
-				}
-				else if (i%3==0){
-					$('#build').append('<p>Fizz<p>');
-				}
-				else if (i%5 == 0){
-					$('#build').append('<p>Buzz<p>');
-				}
-				else{
-					$('#build').append('<p>'+i+'<p>');
-				}
-			};
-			$('#entry').val("");
+		else{
+			for(i=start; i <= end; i++){
+					if (i%3 == 0 && i%5 == 0){
+						$('#build').append('<p>FizzBuzz<p>');
+					}
+					else if (i%3==0){
+						$('#build').append('<p>Fizz<p>');
+					}
+					else if (i%5 == 0){
+						$('#build').append('<p>Buzz<p>');
+					}
+					else{
+						$('#build').append('<p>'+i+'<p>');
+					}
+				};
 		};
+		$('#entry').val("");
+		
+	};
 
+	$("#start").click (function(){
+		fizzy()	
+	});
+
+	$('#inner').keypress(function(e){
+	if(e.which == 13){	
+		fizzy()
+	};
     });
 
 
-	});
+});
 	
 		
 		
